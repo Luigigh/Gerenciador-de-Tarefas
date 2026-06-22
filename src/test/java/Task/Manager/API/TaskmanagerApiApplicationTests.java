0273c6e19136dbccfc1cabe7d01e159822dc70c3
Luigigh <luigifalconibruno@gmail.com>
2026-06-22 20:07:10 -0300

    Projeto conectado com banco em sql no WorkBench com sucesso

---
+++ src/test/java/Task/Manager/API/TaskmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Task.Manager.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

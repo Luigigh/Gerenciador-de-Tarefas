10dd3002ed1efec79d08fa6373997197e058e6da
Luigigh <luigifalconibruno@gmail.com>
2026-07-20 15:40:51 -0300

    feat: CORS permission for front-end integration.

--- src/main/java/br/com/luigifalconi/taskmanager/config/SecurityConfig.java
+++ src/main/java/br/com/luigifalconi/taskmanager/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package br.com.luigifalconi.taskmanager.config;
 import br.com.luigifalconi.taskmanager.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
 
 @Configuration
 public class SecurityConfig {
@@ -24,6 +27,8 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+
+                .cors(Customizer.withDefaults())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
